4fda29436296a680aaf66157b1c0005872c8967a
meeko <meeko@meekos-MacBook-Pro.local>
2020-08-17 22:11:36 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
+import React, {useState, createRef} from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+function HistoryList({list}) {
+		const listItems = list.map(x => <li>{x}</li>)
+		return <ol>{listItems}</ol>
+}
 function App() {
+		const textInput = createRef()
+		const [text, setText] = useState()
+		const [history, setHistory] = useState([])
+		const handleClick = () => {
+				setText(textInput.current.value.toUpperCase())
+				setHistory([...history, textInput.current.value])
+		}
+		const handleBack = () => {
+				setText(history.pop())
+				setHistory(history)
+		}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+		<button onClick={handleBack}>Back</button>
+		<input type="text" ref={textInput}/>
+		<input type="submit" onClick={handleClick}/>
+		<input type="text" value={text}/>
+		<HistoryList list={history}></HistoryList>
     </div>
   );
 }
