ba06b25b12536b5a1c18cda8663aaa8c5bcda8a0
meekoder <meekolydm@yahoo.com>
2020-08-20 16:42:07 -0700

    refactored and fixed spacing

--- src/App.js
+++ src/App.js
@@ -17,23 +17,19 @@ function App() {
 	const [text, setText] = useState()
 	const [history, setHistory] = useState([])
 	const [defaultValue, setDefaultValue] = useState(text);
-	const upperCase = () => {
-		const input = textInput.current.value.toUpperCase()
-		setText(input)
-		setHistory([...history, input])
-	}
-	const lowerCase = () => {
-		const input = textInput.current.value.toLowerCase()
-		setText(input)
-		setHistory([...history, input])
-	}
-	const reverse = () => {
-		const input = [...textInput.current.value].reverse().join('')
-		setText(input)
-		setHistory([...history, input])
-	}
-	const titleCase = () => {
-		const input = textInput.current.value.split(' ').map(x => x[0].toUpperCase() + x.slice(1)).join(' ') 
+
+	const transformer = str => {
+		if (textInput.current.value.length < 1)	return alert('Put some text in the text box!')
+		let input;
+		if (str === 'upper') {
+			input = textInput.current.value.toUpperCase()
+		} else if (str === 'lower') {
+			input = textInput.current.value.toLowerCase()
+		} else if (str === 'title') {
+			input = textInput.current.value.split(' ').map(x => x[0].toUpperCase() + x.slice(1)).join(' ') 
+		} else if (str === 'rev'){
+			input = [...textInput.current.value].reverse().join('')
+		}
 		setText(input)
 		setHistory([...history, input])
 	}
@@ -51,40 +47,37 @@ function App() {
 	const clearHistory = () => {
 		setHistory([])
 	}
+
 	return (
 		<div className="App">
 			<section className="hero is-link is-fullheight">
 				<div className="hero-body">
-					<div className="container" id="text">
-						<h1 className="title">T3xT tR@n5f0rMeR</h1>
-						<div>
-							<div>
-								<div className="back-reset">
-									<button className="button is-small" id="back" onClick={handleBack}><FontAwesomeIcon icon={faArrowLeft} size="2x"/></button>
-									<button className="button is-small" onClick={reset}><strong>Reset Input</strong></button>
-								</div>
-								<textarea className="textarea is-medium" placeholder="what text do you want to transform?" id="textbox" ref={textInput} value={text} onChange={e => setText(e.target.value)}></textarea>
+					<div className="main">
+						<div className="container">
+							<h1 className="title">T3xT tR@n5f0rMeR</h1>
+							<div className="back-reset">
+								<button className="button is-small" onClick={handleBack}><FontAwesomeIcon icon={faArrowLeft} size="2x"/></button>
+								<button className="button is-small" onClick={reset}><strong>Reset Input</strong></button>
 							</div>
+							<textarea className="textarea is-medium" placeholder="what text do you want to transform?" ref={textInput} value={text} onChange={e => setText(e.target.value)}></textarea>
 							<div className="buttons">
-								<button className="button is-dark" onClick={upperCase}>UPPER CASE</button>
-								<button className="button is-dark" onClick={lowerCase}>lower case</button>
-								<button className="button is-dark" onClick={titleCase}>Title Case</button>
-								<button className="button is-dark" onClick={reverse}>Reverse</button>
+								<button className="button is-dark" onClick={() => transformer('upper')}>UPPER CASE</button>
+								<button className="button is-dark" onClick={() => transformer('lower')}>lower case</button>
+								<button className="button is-dark" onClick={() => transformer('title')}>Title Case</button>
+								<button className="button is-dark" onClick={() => transformer('rev')}>Reverse</button>
 							</div>
 						</div>
-						<div>
-							<nav className="panel">
-								<p className="panel-heading">History</p>
-								<div className="panel-block" id="history-list">
-									<HistoryList list={history} className="card-content"></HistoryList>
-								</div>
-								<div className="panel-block" id="foot">
-									<footer className="panel-block">
-										<button className="button is-danger is-outlined is-fullwidth" onClick={clearHistory}>Clear History</button>
-									</footer>
-								</div>
-							</nav>
-						</div>
+						<nav className="panel">
+							<p className="panel-heading">History</p>
+							<div className="panel-block" id="history-list">
+								<HistoryList list={history} className="card-content"></HistoryList>
+							</div>
+							<div className="panel-block">
+								<footer className="panel-block">
+									<button className="button is-danger is-outlined is-fullwidth" onClick={clearHistory}>Clear History</button>
+								</footer>
+							</div>
+						</nav>
 					</div>
 				</div>
 			</section>
